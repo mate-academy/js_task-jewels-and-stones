f6c3b1fc0689beb3b4d50fe6e55099269a1ac80c
Пользователь <Sviatoslv>
2019-06-13 16:16:47 +0300

    Fixed solution

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -28,9 +28,7 @@
  * @return {number}
  */
 function numJewelsInStones(jewels, stones) {
-  const jewelsWithoutDigits = jewels.replace(/\d/g, '');
-  const jewelsWithoutSpecialChar = jewelsWithoutDigits.replace(/\W/g, '');
-  const jewelsClean = jewelsWithoutSpecialChar.replace(/_/g, '');
+  const jewelsWithoutSpecialChar = jewels.replace(/[^a-zA-Z]/g, '');
 
   let numberOfJewelStones = 0;
 
@@ -41,7 +39,7 @@ function numJewelsInStones(jewels, stones) {
   }
 
   for (let i = 0; i < stones.length; i++) {
-    if (isLetterInString(jewelsClean, stones[i])) {
+    if (isLetterInString(jewelsWithoutSpecialChar, stones[i])) {
       numberOfJewelStones++;
     }
   }
