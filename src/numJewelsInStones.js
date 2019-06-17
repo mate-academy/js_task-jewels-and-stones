6c2ad3927d6228687f968db8f9defa8a06901bd5
Taisiia <taisiia.pomazan@gmail.com>
2019-06-17 21:52:12 +0300

    sol

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -29,6 +29,18 @@
  */
 function numJewelsInStones(jewels, stones) {
   // write code here
+  let count = 0;
+  for (let i = 0; i < jewels.length; i++) {
+    for (let j = 0; j < stones.length; j++) {
+      if (jewels[i] === stones[j] && jewels[i] !== jewels[i + 1]) {
+        count++;
+      }
+      if (stones.includes('_, !')) {
+        return count;
+      };
+    }
+  }
+  return count;
 }
 
 module.exports = numJewelsInStones;
