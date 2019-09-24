987501a6df2e0368024264d372b0fbfc360f2c0c
Celestine <celesclaks@yahoo.com>
2019-09-24 06:40:26 -1200

    fs_aug19_celestine

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -29,6 +29,21 @@
  */
 function numJewelsInStones(jewels, stones) {
   // write code here
+  const result = [];
+  const letterJewels = jewels.match(/[a-zA-Z]/g);
+  if (jewels === '' || stones === '') {
+    return 0;
+  }
+  for (let i = 0; i < stones.length; i++) {
+    if (letterJewels.includes(stones[i])) {
+      result.push(stones[i]);
+    }
+    if (result.toString() === '') {
+      return 0;
+    }
+  }
+
+  return result.length;
 }
 
 module.exports = numJewelsInStones;
