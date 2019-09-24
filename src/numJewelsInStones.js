a5567754ee310bfcb50c8f318b7a313823015568
VadimK <djvadya16@gmail.com>
2019-09-24 17:24:55 +0300

    done

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -28,7 +28,13 @@
  * @return {number}
  */
 function numJewelsInStones(jewels, stones) {
-  // write code here
+  let coinsidenceCount = 0;
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.includes(stones[i]) && stones[i].match(/[a-z]/gi)) {
+      coinsidenceCount++;
+    }
+  }
+  return coinsidenceCount;
 }
 
 module.exports = numJewelsInStones;
