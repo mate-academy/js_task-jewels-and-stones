bbf1a18335e91ae0deddfa79ba50c35610a05451
nikakarelets <v.karelets@gmail.com>
2019-09-24 15:16:22 +0300

    Solution

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -28,7 +28,15 @@
  * @return {number}
  */
 function numJewelsInStones(jewels, stones) {
-  // write code here
+  let count = 0;
+
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.includes(stones[i]) && stones[i].match(/[a-z]/ig)) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = numJewelsInStones;
