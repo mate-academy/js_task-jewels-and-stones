57e1bdeaefca47adf9ede5837fcb14eb329d3ad9
Oleksandr Kirsanov <olek.kirsanov@gmail.com>
2019-09-26 13:48:06 +0300

    'Update-with-match'

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -31,20 +31,12 @@ function numJewelsInStones(jewels, stones) {
   let counter = 0;
   const jewelsArr = jewels.split('');
   const jewelsArrUnique = [];
-  const letters = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-  ];
 
   for (let x = 0; x < jewels.length; x++) {
-    for (let y = 0; y < 52; y++) {
-      if (
-        !jewelsArrUnique.includes(jewelsArr[x]) && jewelsArr[x] === letters[y]
-      ) {
-        jewelsArrUnique.push(jewelsArr[x]);
-      }
+    if (
+      !jewelsArrUnique.includes(jewelsArr[x]) && jewelsArr[x].match(/[a-zA-Z]/)
+    ) {
+      jewelsArrUnique.push(jewelsArr[x]);
     }
   }
   const stonesArr = stones.split('');
@@ -56,6 +48,7 @@ function numJewelsInStones(jewels, stones) {
       }
     }
   }
+
   return counter;
 }
 
