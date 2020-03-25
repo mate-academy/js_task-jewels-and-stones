5bbba088005a339355f0148f15e46d602207519c
Tetiana <ishchuktr@gmail.com>
2020-03-25 22:26:04 +0200

    solution

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -28,7 +28,15 @@
  * @return {number}
  */
 function numJewelsInStones(jewels, stones) {
-  // write code here
+  let count = 0;
+
+  (stones.match(/[a-zA-Z]/g) || []).map(el => {
+    if (jewels.includes(el)) {
+      count++;
+    }
+  });
+
+  return count;
 }
 
 module.exports = numJewelsInStones;
