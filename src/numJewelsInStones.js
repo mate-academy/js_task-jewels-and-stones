9e85eef33b07d97f1b881823147cce230fe395d7
YuriiMatsiv <matsiv2017@ukr.net>
2020-03-30 23:31:51 +0300

    solution

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -28,7 +28,19 @@
  * @return {number}
  */
 function numJewelsInStones(jewels, stones) {
-  // write code here
-}
+  let count = 0;
+
+  for (const stone of stones) {
+    if (stone.toLocaleLowerCase() === stone.toUpperCase()) {
+      continue;
+    }
+
+    if (jewels.includes(stone)) {
+      count++;
+    }
+  }
+
+  return count;
+};
 
 module.exports = numJewelsInStones;
