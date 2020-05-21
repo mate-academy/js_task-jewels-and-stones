47da3ee9a08d4bede7f46e7f939f18c3f81110a8
Valentyn Krushynin <krushunin89@gmail.com>
2020-05-21 17:43:37 +0300

    solution

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -26,7 +26,17 @@
  * @return {number}
  */
 function numJewelsInStones(jewels, stones) {
-  // write code here
+  let count = 0;
+
+  for (let i = 0; i < jewels.length; i++) {
+    for (let j = 0; j < stones.length; j++) {
+      if (jewels[i] === stones[j]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
 }
 
 module.exports = numJewelsInStones;
