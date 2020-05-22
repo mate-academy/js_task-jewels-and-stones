e685aa079925c11adb8eb99882dad6a50103a8d7
VlaDinter <vladyslavdinter@gmail.com>
2020-05-22 03:38:38 +0300

    Solution

--- src/numJewelsInStones.js
+++ src/numJewelsInStones.js
@@ -1,32 +1,17 @@
 'use strict';
 
-/**
- * You're given 2 strings:
- *  - jewels - representing the types of stones that are jewels,
- *  - stones - representing all the stones you have.
- *
- * You want to know how many of the stones you have are also jewels.
- *
- * Example 1:
- * Input: jewels = "aA", stones = "aAAbbbb"
- * Output: 3
- *
- * Example 2:
- * Input: jewels = "z", stones = "ZZ"
- * Output: 0
- *
- * Notes:
- *  - stones and jewels will contain only letters and have length at most 50.
- *  - The characters in jewels are unique.
- *  - "a" is considered a different type of stone from "A"
- *
- * @param {string} jewels
- * @param {string} stones
- *
- * @return {number}
- */
 function numJewelsInStones(jewels, stones) {
-  // write code here
+  let letterCount = 0;
+
+  for (const stonesLetter of stones) {
+    for (const jewelsLetter of jewels) {
+      if (stonesLetter === jewelsLetter) {
+        letterCount++;
+      }
+    }
+  }
+
+  return letterCount;
 }
 
 module.exports = numJewelsInStones;
